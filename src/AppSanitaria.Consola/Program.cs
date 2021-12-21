57d467b4c688c3ca65a9f749d4d1b78961ecfbd7
srlopez <srlopezh@gmail.com>
2021-12-21 18:34:53 +0100

    Algo más

--- src/AppSanitaria.Consola/Program.cs
+++ src/AppSanitaria.Consola/Program.cs
@@ -2,7 +2,7 @@ using Sanitaria;
 using Sanitaria.UI.Consola;
 
 
-// Más sencillo no puede quedar
+// Más sencillo no puede
 var view = new Vista();
 var sistema = new GestorDeUrgencias();
 var controlador = new Controlador(view, sistema);
